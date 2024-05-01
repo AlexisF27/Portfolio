2ab37e22c3036f7b6b7f34e10bcfb34c0b31d572
Alexis Fernando Rodriguez <alexisf_27@hotmail.com>
2024-05-01 18:53:01 +0100

    Add animated to the github section

--- public/github_script.js
+++ public/github_script.js
@@ -2,11 +2,18 @@ let languageBoxes = [];
 
 document.addEventListener('DOMContentLoaded', function () {
   var languageBoxes = document.querySelectorAll('.language-box');
+  languageBoxes.forEach(function (element) {
+    element.classList.add('animate__animated');
+    element.classList.add('animate__heartBeat');
+  });
 
   languageBoxes.forEach(function (element) {
     element.addEventListener('mouseover', function () {
-      // Add a class to the hovered element
-      // Remove the class from all other elements
+      element.classList.remove('animate__heartBeat');
+      element.firstElementChild.classList.add('animate__animated');
+
+      element.firstElementChild.classList.add('animate__flip');
+
       languageBoxes.forEach(function (box) {
         if (box !== element) {
           box.classList.add('blur');
@@ -18,7 +25,8 @@ document.addEventListener('DOMContentLoaded', function () {
     element.addEventListener('mouseout', function () {
       // Remove the class when the mouse moves out of the element
       languageBoxes.forEach(function (box) {
-          box.classList.remove('blur');
+        box.classList.remove('blur');
+        box.firstElementChild.classList.remove('animate__flip');
       });
     });
   });
